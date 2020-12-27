dedc412ecb272014d01d8675544fa58c293bb7d7
Urja Choudhary <urja77@gmail.com>
2020-12-27 17:50:57 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I Am Poor'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+          child: Image(
+        image: AssetImage('images/poor.jpg'),
+      )),
+    ),
+  ));
+}
